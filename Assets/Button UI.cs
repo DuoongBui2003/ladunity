3e63f098ec67dc78d9f16740b186614dc775bc30
BuiQuiDuong <duongb2062003@gmail.com>
2025-02-20 18:36:17 +0700

    first commit

---
+++ Assets/Button UI.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+ using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonUI : MonoBehaviour
+{
+    public RectTransform menu; // Kéo menu UI vào đây
+    public Button toggleButton; // Kéo Button vào đây
+    private bool isOpen = false;
+
+    public Vector2 openPosition = new Vector2(0, 0); // Vị trí mở
+    public Vector2 closedPosition = new Vector2(0,2000); // Vị trí đóng
+    public float moveDuration = 0.5f; // Thời gian di chuyển
+    private void Awake()
+    {
+        toggleButton.onClick.AddListener(ToggleMenu);
+    }
+    void Start()
+    {
+       
+    }
+
+    public void ToggleMenu()
+    {
+        isOpen = !isOpen;
+        StopAllCoroutines();
+        StartCoroutine(MoveMenu(isOpen ? openPosition : closedPosition));
+    }
+
+    IEnumerator MoveMenu(Vector2 targetPosition)
+    {
+        Vector2 startPosition = menu.anchoredPosition;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < moveDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            menu.anchoredPosition = Vector2.Lerp(startPosition, targetPosition, elapsedTime / moveDuration);
+            Debug.Log("Menu Position: " + menu.anchoredPosition); // In vị trí menu
+            yield return null;
+        }
+
+        menu.anchoredPosition = targetPosition;
+        Debug.Log("Menu Position Final: " + menu.anchoredPosition); // In vị trí cuối cùng
+    }
+
+}
---
+++ Assets/UIManger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManger : MonoBehaviour
+{
+    public Image cooldown;
+    public bool coolingDown;
+    public float waitTime = 10.0f;
+    public GameObject PanelLoading;
+    public GameObject PanelGame;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+
+    }
+    
+
+    
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if (coolingDown == true)
+        {
+            Debug.Log("fill");
+            //Reduce fill amount over 30 seconds
+            cooldown.fillAmount -= 1.0f / waitTime * Time.deltaTime;
+            if(cooldown.fillAmount <= 0f)
+            {
+                coolingDown = false;
+                PanelLoading.SetActive(false);
+            }
+        }
+
+    }
+    
+
+
+}
